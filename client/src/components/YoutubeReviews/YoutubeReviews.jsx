fb5d925cfb60372de81fe3fe87f736bb0cccba74
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2023-12-28 22:36:38 +0200

    added youtube preview component

---
+++ client/src/components/YoutubeReviews/YoutubeReviews.jsx
@@ -0,0 +1,24 @@
+import youtubeBtn from "../../assets/images/youtube-btn.png";
+// import youtubeBgn from "../../assets/images/youtube.img.jpg";
+import styles from "./YoutubeReviews.module.css";
+
+const YoutubeReviews = () => {
+  return (
+    <section className={styles.youtube}>
+      <div className={styles.content}>
+        <div className={styles.text_youtube}>
+          <span className={styles.subtitle}>youtube reviews</span>
+          <h2 className={styles.title}>
+            Discover detailed, expert car reviews on our YouTube channel.
+            Explore each vehicle.
+          </h2>
+        </div>
+        <div className={styles.picture_youtube}>
+          <img src={youtubeBtn} alt="Zonin Auto Youtube Channel Play Button" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default YoutubeReviews;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import HomeHero from "./Hero/HomeHero";
 import Features from "../../components/Features/Features";
 import About from "../../components/About/About";
+import YoutubeReviews from "../../components/YoutubeReviews/YoutubeReviews";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <HomeHero />
       <Features />
       <About />
+      <YoutubeReviews />
     </>
   );
 };
