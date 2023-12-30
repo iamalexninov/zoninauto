16d10d22d249a1ac3f147835246a658e9c5c2ea5
Alex Georgiev Ninov <iamalexgninov@gmail.com>
2023-12-30 22:37:40 +0200

    added service component

---
+++ client/src/components/Service/Service.jsx
@@ -0,0 +1,46 @@
+// Styles
+import styles from "./Service.module.css";
+
+// Icons
+import { FaCartShopping as Shop } from "react-icons/fa6";
+import { IoCarSport as Dealership } from "react-icons/io5";
+
+// Images
+import mercedes from "../../assets/images/mercedes.png";
+
+const Service = () => {
+  return (
+    <section className={styles.service}>
+      <div className="wrapper">
+        <h4 className={styles.subtitle}>Our Services</h4>
+        <h3 className={styles.title}>Streamlined Automation Services</h3>
+        <div className={styles.items}>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <div className={styles.icon}>
+                <Shop size={60} />
+              </div>
+              <h4 className={styles.item_title}>Car parts</h4>
+              <p className={styles.item_p}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur vitae tenetur laudantium totam ullam, accusantium deserunt possimus. Quis totam, veritatis dolorem, magnam repellendus debitis repudiandae impedit at, officiis vitae ut.</p>
+            </li>
+          </ul>
+          <div className={styles.img}>
+            <img src={mercedes} alt="Mercedes Benz C63 AMG" />
+          </div>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <div className={styles.icon}>
+                <Dealership size={60} />
+              </div>
+              <h4 className={styles.item_title}>Cars Dealership</h4>
+              <p className={styles.item_p}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur vitae tenetur laudantium totam ullam, accusantium deserunt possimus. Quis totam, veritatis dolorem, magnam repellendus debitis repudiandae impedit at, officiis vitae ut.</p>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Service;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import HomeHero from "./Hero/HomeHero";
+import Service from "../../components/Service/Service";
 import About from "../../components/About/About";
 import YoutubeReviews from "../../components/YoutubeReviews/YoutubeReviews";
 import Companies from "../../components/Companies/Companies";
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <>
       <HomeHero />
+      <Service />
       <About />
       <YoutubeReviews />
       <Companies />
